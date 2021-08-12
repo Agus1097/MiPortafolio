0e0321564f7e880d27aacb2fdd788306c8e16276
Agus1097 <agustinleyes10@gmail.com>
2021-08-12 06:37:17 -0300

    estructura lista

---
+++ js/scroll.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var $navbar = $('#menu');
+    var $items = $('#font-color');
+    var $items2 = $('#font-color2');
+    var $items3 = $('#font-color3');
+    var $items4 = $('#font-color4');
+    var $items5 = $('#font-color5');
+    $(window).scroll(function (event) {
+        var scroll = $(this).scrollTop();
+        if (scroll > 10) {
+            $items.addClass('text-color3');
+            $items2.addClass('text-color3');
+            $items3.addClass('text-color3');
+            $items4.addClass('text-color3');
+            $items5.addClass('text-color3');
+            $navbar.addClass('bg-navbar');
+        } else {
+            $items.removeClass('text-color3');
+            $items2.removeClass('text-color3');
+            $items3.removeClass('text-color3');
+            $items4.removeClass('text-color3');
+            $items5.removeClass('text-color3');
+            $navbar.removeClass('bg-navbar');
+        }
+    });
+
+});
