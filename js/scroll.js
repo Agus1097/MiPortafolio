63343c7b42052d348aa7cd33872356459006f359
Agus1097 <agustinleyes10@gmail.com>
2021-08-18 02:09:01 -0300

    header listo

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     var $navbar = $('#menu');
+    var $name = $('#font-name');
     var $items = $('#font-color');
     var $items2 = $('#font-color2');
     var $items3 = $('#font-color3');
@@ -13,6 +14,7 @@ $(document).ready(function () {
             $items3.addClass('text-color3');
             $items4.addClass('text-color3');
             $items5.addClass('text-color3');
+            $name.addClass('color-name');
             $navbar.addClass('bg-navbar');
         } else {
             $items.removeClass('text-color3');
@@ -20,6 +22,7 @@ $(document).ready(function () {
             $items3.removeClass('text-color3');
             $items4.removeClass('text-color3');
             $items5.removeClass('text-color3');
+            $name.removeClass('color-name');
             $navbar.removeClass('bg-navbar');
         }
     });
