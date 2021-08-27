227b52a5e4c6dc3943df831ac302b4bd91c85fc3
Agus1097 <agustinleyes10@gmail.com>
2021-08-27 10:33:18 -0300

    Modificaciones en header, main y nuevo footer

--- js/scroll.js
+++ js/scroll.js
@@ -9,19 +9,19 @@ $(document).ready(function () {
     $(window).scroll(function (event) {
         var scroll = $(this).scrollTop();
         if (scroll > 10) {
-            $items.addClass('text-color3');
-            $items2.addClass('text-color3');
-            $items3.addClass('text-color3');
-            $items4.addClass('text-color3');
-            $items5.addClass('text-color3');
+            $items.addClass('text-color');
+            $items2.addClass('text-color');
+            $items3.addClass('text-color');
+            $items4.addClass('text-color');
+            $items5.addClass('text-color');
             $name.addClass('color-name');
             $navbar.addClass('bg-navbar');
         } else {
-            $items.removeClass('text-color3');
-            $items2.removeClass('text-color3');
-            $items3.removeClass('text-color3');
-            $items4.removeClass('text-color3');
-            $items5.removeClass('text-color3');
+            $items.removeClass('text-color');
+            $items2.removeClass('text-color');
+            $items3.removeClass('text-color');
+            $items4.removeClass('text-color');
+            $items5.removeClass('text-color');
             $name.removeClass('color-name');
             $navbar.removeClass('bg-navbar');
         }
